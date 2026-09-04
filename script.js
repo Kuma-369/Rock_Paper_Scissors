900be0ae117c04e1591c492f0c51262df9d5a4de
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-04 15:31:30 +0100

    getComputerChoice( ) return interger from 0 to 2.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // FUNCTION getComputerChoice() to random return "rock", "paper", "scissors" for computer side.
-// Generate a random interger from 0 ro 2. 
-// SET getRandomInt(3)
+// SET Math.random with min = 0 and max = 3.
+// Generate a random interger from 0 to 2. 
 // RETURN string according to the interger.
 // IF interger = 0 THEN return string "rock".
 // IF interger = 1 THEN return string "paper".
 // IF interger = 2 THEN return string "scissors".
+
+function getComputerChoice() {
+    const randomInt = Math.floor(Math.random() * 3);
+    return randomInt;
+   // Return interger from 0 to 2 randomly.
+}
+
+console.log(getComputerChoice());
