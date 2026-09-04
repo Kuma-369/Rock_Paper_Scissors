b67d76334a1d1137b913b335efb08bd64fdf8b80
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-04 14:57:04 +0100

    Pseudocode for function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+// FUNCTION getComputerChoice() to random return "rock", "paper", "scissors" for computer side.
+// Generate a random interger from 0 ro 2. 
+// SET getRandomInt(3)
+// RETURN string according to the interger.
+// IF interger = 0 THEN return string "rock".
+// IF interger = 1 THEN return string "paper".
+// IF interger = 2 THEN return string "scissors".
